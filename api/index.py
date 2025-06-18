d65f8b8a04ef5a3163fbb34e7943e888979dfd83
Sia Wang <sia.wang3847@gmail.com>
2025-06-18 15:05:17 +0800

    change import route

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from chatgpt import ChatGPT
+from api.chatgpt import ChatGPT
 from dotenv import load_dotenv
 
 import os
